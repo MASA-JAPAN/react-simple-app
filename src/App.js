4a1f43baa7e7b79ee40745b3415a12c395e95c95
MASA-JAPAN <masaifactoryline@gmail.com>
2018-11-23 19:48:03 +0900

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
 
-    return(
+//     return(
 
-      <React.Fragment>
+//       <React.Fragment>
         
-        <label htmlFor = "bar">bar</label>
-        <input type="text" onChange={()=>{console.log("I am clicked!!!")}}/>
+//         <label htmlFor = "bar">bar</label>
+//         <input type="text" onChange={()=>{console.log("I am clicked!!!")}}/>
         
-      </React.Fragment>
+//       </React.Fragment>
 
 
-    ) 
+//     ) 
 
 
-  }
+//   }
+// }
+
+const App = () => {
+
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+
+
+
+  </div>
+
+}
+
+const Cat = () => {
+
+  return <div>Meow!</div>
+
 }
 
 export default App;
