16dd8bc9a9c37b118ece2228a585250d435a3fb9
MASA-JAPAN <masaifactoryline@gmail.com>
2018-12-05 21:08:52 +0900

    Commit

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,9 +1,9 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux'
 import _ from 'lodash'
+import {Link} from 'react-router-dom'
 
 import { readEvents } from '../actions'
-import { dispatch } from 'rxjs/internal/observable/pairs';
 
 class EventsIndex extends Component {
   componentDidMount(){
@@ -23,17 +23,21 @@ class EventsIndex extends Component {
   render(){
     return(
       <React.Fragment>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {this.renderEvents()}
-        </tbody>
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+
+        <Link to="/events/new">New Event</Link>
       </React.Fragment>
     )
   }
